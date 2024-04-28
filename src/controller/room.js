711ce2ab6d5384da1686d202633392376b6916bf
fauzanramadhani06 <fauzanramadhani06@gmail.com>
2024-04-29 00:38:32 +0700

    feat: Update docs

--- src/controller/room.js
+++ src/controller/room.js
@@ -5,7 +5,7 @@ const getRoomId = async (req, res) => {
     const user = req.user;
     return res.status(200).json({
       status: "success",
-      message: "Get room_id successfully",
+      message: "Get room id successfully",
       data: user.room_id,
     });
   } catch (error) {
